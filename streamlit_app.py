32bce4f9f0a0cc6da4b989df9cef2fff7e230163
shkumbim <kelmendishkumbim@GMAIL.COM>
2023-06-25 13:21:34 +0200

    file upload added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import streamlit as st
 
-df = None
+df=None 
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write('File uploaded')
+
+if st.button('Show list of countries'):
+    if df is None:
+        st.write('Upload the dataframe')
+    else:
+        list_of_countries=df['country'].unique()
+        st.write(list_of_countries)
+
 
 st.title('Creative hub data science project ')
 uploaded_file = st.file_uploader("Choose a file")
